387e441113585616d3f9da15d53c7954eeddd243
Vigneshwar S U <139466241+Vigneshwar-SU@users.noreply.github.com>
2025-09-12 05:41:08 +0000

    try pandrom

--- problem3.py
+++ problem3.py
@@ -1,7 +1,7 @@
 # Problem3: Check if two strings are anagrams (contain the same letters in any order).
 
 def are_anagrams(s1, s2):
-    return sorted(s1) = sorted(s2)
+    return sorted(s1) == sorted(s2)
 
 print(are_anagrams("listen", "silent"))
 print(are_anagrams("hello", "world"))

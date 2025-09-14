60156399faf8f3d0617e02d437211fdaf26e4b54
KavinilaLoganathan-2507 <kavinilaloganathan@gmail.com>
2025-09-14 17:41:11 +0100

    Update problem1.py

--- problem1.py
+++ problem1.py
@@ -4,7 +4,7 @@ def sum_even_numbers(nums):
     total = 0
     for num in nums:
         if num % 2 == 0:
-            total = num
+            total = num + total
     return total
 
 print(sum_even_numbers([1, 2, 3, 4, 5, 6]))

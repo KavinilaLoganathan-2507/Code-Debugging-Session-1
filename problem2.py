e6493dddf01a3328c7eb14df9f9fbc05c983bf25
KavinilaLoganathan-2507 <kavinilaloganathan@gmail.com>
2025-09-14 18:07:50 +0100

    Update problem2.py

--- problem2.py
+++ problem2.py
@@ -1,8 +1,8 @@
 # Problem2: Return the second largest unique number in the list.
 
-def second_largest(nums):
-    nums = list(set(nums))
-    nums.sort()
-    return nums[-2]
+def second_largest(num):
+    num = list(set(num))
+    num.sort()
+    return num[-2]
 
 print(second_largest([4, 1, 7, 7, 3]))

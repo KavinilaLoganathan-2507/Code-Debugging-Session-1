dcbd60e288a2127139f72e11ce05b8d9122c3347
KavinilaLoganathan-2507 <kavinilaloganathan@gmail.com>
2025-09-14 18:12:03 +0100

    Update problem7.py

--- problem7.py
+++ problem7.py
@@ -1,8 +1,8 @@
 # Problem7: Given an array containing n distinct numbers from 0 to n, find the missing number.
 
 def find_missing(nums):
-    n = len(nums)
-    expected_sum = n * (n + 1) / 2
+    num = len(nums)
+    expected_sum = num * (num + 1) // 2
     actual_sum = sum(nums)
     return expected_sum - actual_sum
 

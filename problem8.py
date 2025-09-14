3bead5180a6f4fca18936e45343e734b89c6ff71
KavinilaLoganathan-2507 <kavinilaloganathan@gmail.com>
2025-09-14 18:11:22 +0100

    Update problem8.py

--- problem8.py
+++ problem8.py
@@ -1,9 +1,9 @@
 # Problem8: Rotate a square matrix by 90 degrees clockwise.
 
 def rotate(matrix):
-    n = len(matrix)
-    for i in range(n):
-        for j in range(i, n):
+    num = len(matrix)
+    for i in range(num):
+        for j in range(i, num):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
     matrix.reverse()
     return matrix

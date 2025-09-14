7a780ec14371028f7a993deffc1196ddc0a4de63
KavinilaLoganathan-2507 <kavinilaloganathan@gmail.com>
2025-09-14 18:14:31 +0100

    Update problem5.py

--- problem5.py
+++ problem5.py
@@ -6,7 +6,7 @@ def merge_intervals(intervals):
     for current in intervals[1:]:
         last = merged[-1]
         if current[0] <= last[1]:
-            last[1] = current[1]
+            last[1] = max(last[1], current[1]
         else:
             merged.append(current)
     return merged
